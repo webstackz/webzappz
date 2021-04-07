b56b5b9609c0b386cfbd055fbca06d0eae6db562
www.arunsystem@gmail.com <35886419+codeneed@users.noreply.github.com>
2021-04-07 13:22:48 +0530

    js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+app.get('/', (req, res) => {
+    res.send('hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
